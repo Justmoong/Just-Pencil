08869f95a6e098af31cbaa8814c8b05d1bb5dd84
Justmoong <andudyun0504@gmail.com>
2025-02-24 00:21:27 +0900

    Initial Commit

---
+++ Just Pencil/Just_PencilApp.swift
@@ -0,0 +1,17 @@
+//
+//  Just_PencilApp.swift
+//  Just Pencil
+//
+//  Created by ymy on 2/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Just_PencilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
